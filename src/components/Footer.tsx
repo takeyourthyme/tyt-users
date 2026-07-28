951ce073d970136badd841487f02520df08aca6b
takeyourthyme <takeyourthymebr@gmail.com>
2026-07-28 20:04:12 +0100

    chore: remove transactional email disclaimer from footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,7 +45,7 @@ const Footer = ({ variant }: FooterProps) => {
           </span>
 
           <span>
-            © 2026 Take Your Thyme - Todos os direitos reservados. Nome Fantasia TYT - CNPJ 57.310.363/0001-40 - Rua Mario Prandini, 775 - Centro, Itapeva - SP, 18.400-170. Você recebeu este e-mail porque realizou uma transação na plataforma Take Your Thyme. Este é um e-mail transacional obrigatório.
+            © 2026 Take Your Thyme - Todos os direitos reservados. Nome Fantasia TYT - CNPJ 57.310.363/0001-40 - Rua Mario Prandini, 775 - Centro, Itapeva - SP, 18.400-170.
           </span>
         </div>
       </div>
